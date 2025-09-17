97042320c42c19d1f0325ca6324df5fde4a20e90
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-09-17 21:05:11 +0600

    layout, structure, header, theme somewhat done

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Header from '../components/Header/Header';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header>
+                <Header></Header>
+            </header>
+
+            <main>
+                <section className="left-section"></section>
+
+                <section className="main-section">
+                    <Outlet></Outlet>
+                </section>
+
+                <section className="right-section"></section>
+            </main>
+        </div>
+    );
+};
+
+export default HomeLayout;
