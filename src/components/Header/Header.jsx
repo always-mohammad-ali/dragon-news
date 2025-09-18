3ce4535e4a41c47860c2090021ae4e9eda67e77d
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-09-18 09:18:14 +0600

    date assigned from date fns library

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './../../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex flex-col gap-4 justify-center items-center'>
             <img src={logo} alt="" />
-            <p>Journalism Without Fear or Favour</p>
+            <p className='text-secondary'>Journalism Without Fear or Favour</p>
+            <p className='text-secondary'>{format(new Date(), "EEEE , MMMM dd yyyy")}</p>
         </div>
     );
 };
