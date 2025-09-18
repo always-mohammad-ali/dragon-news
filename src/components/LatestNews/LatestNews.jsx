b9f7b413cd6d8669413eaf9c73a3898f7a1cf85e
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-09-18 11:03:02 +0600

    marquees add done

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -1,10 +1,20 @@
+import Marquee from "react-fast-marquee";
 
 
 const LatestNews = () => {
     return (
         <div className='bg-gray-200 flex items-center gap-5 p-3 mx-auto my-5'>
             <p className='bg-accent py-2 px-3 text-white'>Latest</p>
-            <p className="font-semibold text-sm">Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</p>
+
+            <Marquee className="flex gap-9" pauseOnHover={true} speed={100}>
+                 <p className="font-semibold text-sm">Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</p>
+
+                 <p className="font-semibold text-sm">Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</p>
+
+                 <p className="font-semibold text-sm">Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</p>
+
+            </Marquee>
+            
         </div>
     );
 };
