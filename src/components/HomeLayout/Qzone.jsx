63f5711de09c48fe9de9ba25f88143e14a80ce4e
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-09-19 18:44:15 +0600

    Qzone.jsx

--- src/components/HomeLayout/Qzone.jsx
+++ src/components/HomeLayout/Qzone.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import swimmming from './../../assets/swimming.png';
+import playground from './../../assets/playground.png';
+import classImg from './../../assets/class.png';
+
 
 const Qzone = () => {
     return (
         <div>
-            
+            <div>
+                <img src={swimmming} alt="" />
+
+            </div>
+            <div>
+                <img src={playground} className='my-4' alt="" />
+            </div>
+            <div>
+                <img src={classImg} alt="" />
+            </div>
         </div>
     );
 };
